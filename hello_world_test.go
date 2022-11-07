e4b55fa5481f5505e96bf9ad089a5697d007a8fc
JalalAlbarn96 <jalal.albarn@gmail.com>
2022-11-07 16:41:46 +0700

    1. Hello World

---
+++ hello_world_test.go
@@ -0,0 +1,13 @@
+package gobyexample
+
+import (
+	"fmt"
+	"testing"
+)
+
+// Our first program will print the classic “hello world” message.
+//Here’s the full source code.
+
+func TestHelloWorld(t *testing.T) {
+	fmt.Println("Hello World")
+}
